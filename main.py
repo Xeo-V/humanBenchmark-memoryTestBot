90529aa8fc12e2d6d2687eddc30f5bef5184eec5
X30 <74484800+Xeo-V@users.noreply.github.com>
2023-10-25 11:27:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import pyautogui
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import time
+import keyboard
+
+GAME_REGION = (654, 170, 1246-654, 673-170)
+LEVEL_REGION = (654, 140, 1246-654, 170-140)
+
+patterns = []
