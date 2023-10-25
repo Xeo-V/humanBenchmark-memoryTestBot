c131df179dbe655892a7d2ed450deb4aedf5e16e
X30 <74484800+Xeo-V@users.noreply.github.com>
2023-10-25 11:28:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,11 @@ patterns = []
 
 def capture_screen(region):
     return pyautogui.screenshot(region=region)
+
+def get_level(region):
+    screenshot = capture_screen(region)
+    screenshot_np = np.array(screenshot)
+    gray_screenshot = cv2.cvtColor(screenshot_np, cv2.COLOR_BGR2GRAY)
+    _, thresholded = cv2.threshold(gray_screenshot, 200, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return len(contours)
